078ec10e0b92ee5f4afcda277c53ef30eadc4209
hiveship <maelig.nantel@gmail.com>
2014-01-25 17:37:34 +0100

    changement de root_path et adaptation des vues users avec bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Recrutement::Application.routes.draw do
 
   resources :candidatures
 
+  root to: 'site#login'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
